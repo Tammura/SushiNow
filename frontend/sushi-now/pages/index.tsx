7fe70952a2712cddac6575639b761e9429fdd145
Ahmed <aamer@red.software.systems>
2023-01-09 15:48:58 +0100

    Create login page

--- frontend/sushi-now/pages/index.tsx
+++ frontend/sushi-now/pages/index.tsx
@@ -1,14 +1,47 @@
-import Head from "next/head";
-import Image from "next/image";
-import { Inter } from "@next/font/google";
-import styles from "../styles/Home.module.css";
-
-const inter = Inter({ subsets: ["latin"] });
+import BaseLayout from "../components/BaseLayout";
+import { useForm } from "react-hook-form";
 
 export default function Home() {
+  const { register, handleSubmit } = useForm();
+  const onSubmit = (data: any) => console.log(data);
+
   return (
     <main>
-      <h1 className="text-3xl">SushiNow will be available soon</h1>
+      <BaseLayout>
+        <div className="relative">
+          {/* Inserisci username */}
+          <form action="" method="post" onSubmit={handleSubmit(onSubmit)}>
+            <div className="flex flex-col justify-center space-y-5 absolute z-10 w-full">
+              <div className="mx-4">
+                <input
+                  {...(register("username"), { required: true })}
+                  type="text"
+                  id="username"
+                  placeholder="Inserisci username"
+                  className="w-full bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg after:block p-2.5"
+                />
+              </div>
+              <div className="mx-4">
+                <input
+                  type="number"
+                  id="tableNumber"
+                  {...(register("tableNumber"), { required: true })}
+                  placeholder="Inserisci il numero del tavolo"
+                  className="w-full bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg after:block p-2.5"
+                />
+              </div>
+              <div className="flex justify-center mx-4">
+                <button
+                  type="submit"
+                  className="w-1/2 text-white bg-primary font-medium rounded-lg text-sm sm:w-auto px-5 py-2.5 text-center"
+                >
+                  Inizia a ordinare
+                </button>
+              </div>
+            </div>
+          </form>
+        </div>
+      </BaseLayout>
     </main>
   );
 }
